70293e1aeea7174b76a95e22ae6a9c366868a64c
beanmoro <bean.moro@gmail.com>
2024-05-09 13:18:04 -0400

    Transferencias agregadas!

--- models/transferencia.model.js
+++ models/transferencia.model.js
@@ -24,14 +24,54 @@ const getLastTen = async()=>{
 
 
 const create = async({descripcion, fecha, monto, cuenta_origen, cuenta_destino})=>{
-    const query = {
-        text: `INSERT INTO transferencias(descripcion, fecha, monto, cuenta_origen, cuenta_destino)
-                VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
-        values: [descripcion, fecha, monto, cuenta_origen, cuenta_destino]
+
+    try {
+        await pool.query('BEGIN')
+
+
+        const cuentaOrigenQuery = {
+            text: 'SELECT * FROM cuentas WHERE id = $1;',
+            values: [cuenta_origen]
+        }
+
+        const cuentaRows = await pool.query(cuentaOrigenQuery);
+        const cuentaOrigen = cuentaRows.rows[0];
+
+        if(parseInt(cuentaOrigen.saldo) < parseInt(monto)){
+            throw Error('Saldo insuficiente!')
+        }
+
+        const updateCuentaOrigenQuery = {
+            text: 'UPDATE cuentas SET saldo = saldo - $2 WHERE id = $1;',
+            values: [cuenta_origen, monto]
+        }
+
+        const updateCuentaDestinoQuery = {
+            text: 'UPDATE cuentas SET saldo = saldo + $2 WHERE id = $1;',
+            values: [cuenta_destino, monto]
+        }
+
+        await pool.query(updateCuentaOrigenQuery);
+        await pool.query(updateCuentaDestinoQuery);
+
+
+        const query = {
+            text: `INSERT INTO transferencias(descripcion, fecha, monto, cuenta_origen, cuenta_destino)
+                    VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
+            values: [descripcion, fecha, monto, cuenta_origen, cuenta_destino]
+        }
+    
+        const {rows} = await pool.query(query);
+        await pool.query('COMMIT')
+        return rows[0];
+        
+    } catch (error) {
+        console.error(error);
+        await pool.query('ROLLBACK')
+        return { error};
     }
 
-    const {rows} = await pool.query(query);
-    return rows[0];
+
 }
 
 
